c967d72f3604764b76d384afcce7d24f9c2c6cef
GG@GG.GG <GLG@GG.GG>
2021-11-12 12:11:13 -0800

    Git ignore added

---
+++ CPU/InstructionDecoder.v
@@ -0,0 +1,8 @@
+`timescale 1ns / 1ps
+//Condition-OPCode-S-Destination-/Src1/Src2/ShiftROR/3bits/-ShiftRORControl
+//4-4-1-4-/4/4/5/3/-3
+// 4/4/5/3 Imidiate value 16 bits
+module InstructionDecoder();
+
+
+endmodule
