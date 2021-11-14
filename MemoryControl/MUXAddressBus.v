86474f071f48367583ed5324a5bebb2d9abe7a11
GG@GG.GG <GLG@GG.GG>
2021-11-14 08:40:23 -0800

    Memory Control Initiatied

---
+++ MemoryControl/MUXAddressBus.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    07:52:30 11/14/2021 
+// Design Name: 
+// Module Name:    MUXAddressBus 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module MUXAddressBus(
+    );
+
+
+endmodule
---
+++ MemoryControl/Memorycontrol.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    07:50:47 11/14/2021 
+// Design Name: 
+// Module Name:    Memorycontrol 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Memorycontrol(
+    );
+
+
+endmodule
