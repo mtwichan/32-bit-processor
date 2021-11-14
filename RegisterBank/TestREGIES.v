319624425241118ac86ec035242a34faba6d31d0
GG@GG.GG <GLG@GG.GG>
2021-11-13 20:57:29 -0800

    Merged?

---
+++ RegisterBank/TestREGIES.v
@@ -0,0 +1,60 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   19:49:40 11/13/2021
+// Design Name:   RegisterBank
+// Module Name:   /home/retr0/VERILOGGClass/RegisterBank/TestREGIES.v
+// Project Name:  RegisterBank
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: RegisterBank
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module TestREGIES;
+
+	// Inputs
+	reg [3:0] dest;
+	reg [31:0] Din;
+	reg [3:0] srcadd1;
+	reg [3:0] srcadd2;
+
+
+	// Instantiate the Unit Under Test (UUT)
+	RegisterBank uut (
+		.dest(dest), 
+		.Din(Din), 
+		.srcadd1(srcadd1), 
+		.srcadd2(srcadd2), 
+		.src1(src1), 
+		.src2(src2)
+	);
+
+	initial begin
+		// Initialize Inputs
+		dest = 0;
+		Din = 0;
+		srcadd1 = 0;
+		srcadd2 = 0;
+
+
+		// Wait 100 ns for global reset to finish
+		#100;
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
+
--- RegisterBank/fuseRelaunch.cmd
+++ RegisterBank/fuseRelaunch.cmd
@@ -1,1 +1,1 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "/home/retr0/VERILOGGClass/RegisterBank/simulationFoRegisters_isim_beh.exe" -prj "/home/retr0/VERILOGGClass/RegisterBank/simulationFoRegisters_beh.prj" "work.simulationFoRegisters" "work.glbl" 
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "/home/retr0/VERILOGGClass/RegisterBank/PLEASEWORK_isim_beh.exe" -prj "/home/retr0/VERILOGGClass/RegisterBank/PLEASEWORK_beh.prj" "work.PLEASEWORK" "work.glbl" 
