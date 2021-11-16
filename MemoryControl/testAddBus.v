3e316190fabbc535994001f6d6869cfc9dc2bd60
CallMeBaguette <94249286+CallMeBaguette@users.noreply.github.com>
2021-11-15 23:09:09 -0800

    updates for clarity

--- MemoryControl/testAddBus.v
+++ MemoryControl/testAddBus.v
@@ -7,14 +7,14 @@ module test_addMux;
 	begin
 	    in0=1'b1; in1=8'b0; in2=16'b0;
 	#10 in0=1'b0; in1=8'b1; in2=16'b0000001100100100;
-	#10 in0=1'b0; in1=8'b0110; in2=16'b0100000100100101;
+	#10 in0=1'b1; in1=8'b0110; in2=16'b0100000100100101;
 	#10 in0=1'b0; in1=8'b1010; in2=16'b0000001100100100;
 	#10 in0=1'b1; in1=8'b10011100; in2=16'b1100001001010110;
 	end
 
 	initial
 	begin
-	$monitor($time," 1st num.%b, 2nd num.=%b, 3rd num.=%b, output=%b",in0,in1,in2,result);
+	$monitor($time," Select=%b, PC instr=%b, Address bus in=%b, address bus out=%b",in0,in1,in2,result);
 	end
 
 	MUXAddressBus bus(in0,in1,in2,result);
