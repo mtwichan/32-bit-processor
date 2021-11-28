979981f537c9078a8dd5a07a48aa99017e788cb0
matthewchan <mtwichan@gmail.com>
2021-11-27 19:13:05 -0800

    fix: update processor test

--- testBench/testProcessor.v
+++ testBench/testProcessor.v
@@ -1,25 +1,28 @@
+`timescale 1 ns/10 ps
 module test_processor;
+	// This test bench simulates the program counter fetch cycle and instruction execution
 
+	localparam period = 10; // 10 * timescale = 10 * 1 ns  = 10ns
 
-    Ram Test_Ram(.read_write(read_write), .address(address), .fetch_address(fetch_address), .data_in(data_in), .data_out(data_out), .fetch_out(fetch_out));
+	reg [15:0] fetch_address;
 
-    // load instruction set into memory
-	initial
+	initial // TODO: Make it so that we can dynamically set the # of fetch cycles OR for loop
 	begin
-
-		$readmemb("TestData/test_ram_read_alu.txt", Test_Ram.mem); 
-		read_write = 0;		address = 15'd0;	fetch_address = 15'd0;  
-	#5	read_write = 1;		address = 15'd0;	fetch_address = 15'd0;
-	#5	read_write = 1;		address = 15'd1;	fetch_address = 15'd3;
-	#5	read_write = 1;		address = 15'd2;	fetch_address = 15'd2;
-	#5	read_write = 1;		address = 15'd3;	fetch_address = 15'd1;
-
+				fetch_address = 15'd0;  
+		#period	fetch_address = 15'd1;
+		#period	fetch_address = 15'd2;
+		#period	fetch_address = 15'd3;
+		#period	fetch_address = 15'd4;
+		#period	fetch_address = 15'd5;
+		#period	fetch_address = 15'd6;
+		#period	fetch_address = 15'd7;
 	end
 
 	initial
 	begin
-
-	$monitor($time, "Address: %d FetchAddress: %d DataOut: %b FetchDataOut: %b", address, fetch_address, data_out, fetch_out);
-
+		$monitor($time, "FetchAddress: %d" , fetch_address);
 	end
+
+	Processor processor(.fetch_address(fetch_address));
+
 endmodule
