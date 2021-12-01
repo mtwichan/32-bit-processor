447b9f379d88442647c5add8e969a31537c722bd
matthewchan <mtwichan@gmail.com>
2021-11-30 19:42:28 -0800

    fix: fix bugs and print out result

--- MemoryControl/MUXAddressBus.v
+++ MemoryControl/MUXAddressBus.v
@@ -1,6 +1,7 @@
 module MUXAddressBus(sel_add_bus, address_add_bus_in, address_add_bus_out, pc_addr);
 	
 	input sel_add_bus; // Select (LDR=1 OR STR=1)
+	input [7:0] pc_addr;
 	input [15:0] address_add_bus_in; 
 	output reg [15:0] address_add_bus_out; 
 	
@@ -12,7 +13,7 @@ module MUXAddressBus(sel_add_bus, address_add_bus_in, address_add_bus_out, pc_ad
 				end
 			else
 				begin
-					address_add_bus_out <= pc_addr
+					address_add_bus_out <= pc_addr;
 				end
 		end
 endmodule
